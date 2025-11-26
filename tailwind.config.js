9d72c00111af476c2c43cd726fc5423ceec3020d
Eric Bowser <ericryanbowser@gmail.com>
2025-11-26 01:45:30 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: "class",
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                burtons: ['../src/assets/fonts/Burtons.otf']
+            }
+        },
+    },
+    plugins: [],
+}
